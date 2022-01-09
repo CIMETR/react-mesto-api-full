aa3da5f7429d45cc3676dc8366a7f1ede84622fc
CIMETR <82894158+CIMETR@users.noreply.github.com>
2022-01-09 16:22:23 +0300

    15beck

---
+++ frontend/src/components/PopupWithForm.js
@@ -0,0 +1,32 @@
+
+function PopupWithForm({ name, title, isOpen, children, onSubmit, buttonText, onClose }) {
+  return (
+    <div className={`popup ${isOpen ? 'popup_opened' : ''}`}>
+      <div className="popup__container popup__overlay">
+        <form
+        className="popup__form"
+        name={name}
+        noValidate
+        onSubmit={onSubmit}
+        >
+          <h2 className="popup__title">{title}</h2>
+          {children}
+          <button
+            type="submit"
+            className="popup__button"
+            >
+              {buttonText}
+            </button>
+        </form>
+        <button
+          type="button"
+          className="popup__close-button"
+          onClick={onClose}
+        >
+        </button>
+      </div>  
+    </div>
+  )
+}
+
+export default PopupWithForm
